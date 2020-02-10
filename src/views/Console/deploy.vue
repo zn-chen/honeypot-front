99d3e3d32ac43248c6b878e853a54222af7c0fe1
root <root@localhost.localdomain>
2020-02-10 08:21:06 -0500

    完成部署页面

--- src/views/Console/deploy.vue
+++ src/views/Console/deploy.vue
@@ -2,55 +2,39 @@
   <div>
     <br />
     <br />
-    <el-form ref="form" :model="form" label-width="80px">
-      <el-form-item label="活动名称">
-        <el-input v-model="form.name"></el-input>
+    <el-form ref="form" :model="form" label-width="80px" :rules="rules">
+      <el-form-item label="节点名称" prop="name">
+        <el-input v-model="form.name" style="width:500px"></el-input>
       </el-form-item>
 
-      <el-form-item label="活动区域">
-        <el-select v-model="form.region" placeholder="请选择活动区域">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
+      <el-form-item label="节点类型" prop="type">
+        <el-select v-model="form.type" placeholder="请选择节点类型">
+          <el-option label="SSH仿真蜜罐" value="cowrie"></el-option>
+          <el-option label="WEB渗透蜜罐" value="none"></el-option>
         </el-select>
       </el-form-item>
 
-      <el-form-item label="活动时间">
-        <el-col :span="11">
-          <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-        </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
-          <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-        </el-col>
-      </el-form-item>
-
-      <el-form-item label="即时配送">
-        <el-switch v-model="form.delivery"></el-switch>
-      </el-form-item>
-
-      <el-form-item label="活动性质">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-          <el-checkbox label="地推活动" name="type"></el-checkbox>
-          <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-          <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-
-      <el-form-item label="特殊资源">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="线上品牌商赞助"></el-radio>
-          <el-radio label="线下场地免费"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-
-      <el-form-item label="活动形式">
-        <el-input type="textarea" v-model="form.desc"></el-input>
+      <el-form-item label="节点描述" prop="des">
+        <el-input
+          type="textarea"
+          :autosize="{ minRows: 2, maxRows: 30}"
+          v-model="form.des"
+          style="width:500px"
+        ></el-input>
       </el-form-item>
 
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">立即创建</el-button>
-        <el-button>取消</el-button>
+        <el-button type="primary" @click="onSubmit('form')">获取部署命令</el-button>
+        <el-button @click="resetForm('form')">重置</el-button>
+      </el-form-item>
+
+      <el-form-item label="部署命令">
+        <el-input
+          type="textarea"
+          :autosize="{ minRows: 5, maxRows: 30}"
+          v-model="deploymentCommand"
+          style="width:500px"
+        ></el-input>
       </el-form-item>
     </el-form>
   </div>
@@ -62,19 +46,44 @@ export default {
     return {
       form: {
         name: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: ""
+        type: "",
+        des: ""
+      },
+      deploymentCommand: "",
+      rules: {
+        name: [{ required: true, message: "请输入节点名称", trigger: "blur" }],
+        type: [
+          { required: true, message: "请选择蜜罐类型", trigger: "change" }
+        ],
+        des: [{ required: true, message: "请输入节点描述", trigger: "blur" }]
       }
     };
   },
   methods: {
-    onSubmit() {
-      window.console.log("submit!");
+    onSubmit(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.submit();
+        } else {
+          alert("参数缺失或格式错误");
+          return false;
+        }
+      });
+    },
+    submit() {
+      this.axios.post(process.env.VUE_APP_BASEURL + "/deploy", this.form).then(
+        res => {
+          this.deploymentCommand = res.data.result;
+          window.console.log(this.deploymentCommand);
+        },
+        res => {
+          // 错误回调
+          window.console.log(res);
+        }
+      );
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
     }
   }
 };
